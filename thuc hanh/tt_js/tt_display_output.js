d53caa0a27e4436290323a07fbf1d8cdbbe73e05
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-04-08 00:28:53 +0700

    th_display_output

---
+++ thuc hanh/tt_js/tt_display_output.js
@@ -0,0 +1,10 @@
+function showMess(){
+    document.getElementById("demo").innerHTML="hello world get ele by id"
+
+    alert("hello world, alert")
+
+    document.write("hello world, write")
+
+    console.log("hello world, consonle")
+}
+
