3264ec7f363a880cd4ff33a74f2e0c57bfeabbd8
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-04-09 12:59:45 +0700

    updated

--- thuc hanh/tt_js/th_display_output.js
+++ thuc hanh/tt_js/th_display_output.js
@@ -1,10 +1,23 @@
+// function showMess(){
+//     // document.getElementById("demo").innerHTML="hello world get ele by id"
+//     //
+//     // alert("hello world, alert")
+//     //
+//     // document.write("hello world, write")
+//     //
+//     // console.log("hello world, consonle")
+//
+//
+//     // alert("Hello world")
+// }
+
+// function mouseOver(){
+//     document.getElementById("demo_mouseover").style.color="red";
+// }
+// function  mouseOut(){
+//     document.getElementById("demo_mouseover").style.color="black";
+// }
 function showMess(){
-    document.getElementById("demo").innerHTML="hello world get ele by id"
-
-    alert("hello world, alert")
-
-    document.write("hello world, write")
-
-    console.log("hello world, consonle")
+    document.getElementById("demo").innerHTML = "helo c03"
 }
 
